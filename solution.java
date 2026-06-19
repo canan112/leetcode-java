713c925fb2ea2e1f3b892e06710a38e0cb0dfaae
Canan Kılıç <canankilic3465@gmail.com>
2026-06-19 09:04:43 +0300

    eksikler giderildi
    Refactor twoSum method to improve efficiency by avoiding duplicate checks.

--- solution.java
+++ solution.java
@@ -7,18 +7,32 @@ class Solution {
     
         for(int i=0; i<nums.length-1; i++)
         {
-            for(int j=nums.length-1; j>-1; j--)
+            for(int j=i+1; j<nums.length; j++)
             {
                 
             
                     if(nums[i]+nums[j]==target)
                     {
-                        return new int[]{i, j};
+                    if(target-nums[i]==nums[j])
+                        {
+                            return new int[]{i, j};
+                        }
+                        
                     }
                 
             }
-            
+        
           
+            
+        }
+          return new int[]{};
+      
+    }
+}  
+            
+
+
+
                 
 
 
